946d7462d4917ec855a7caaa14669eff67353634
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-08-25 12:20:49 -0700

    app up and running

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports={
+    credentials:{
+        client_id:"9bX6cxn5YopXqioI2w2YoI9qrbubnOSs",
+        client_secret:"U4df404acb18446d"
+    },
+    scopes:{
+        internal: ['bucket:create','bucket:read','data:read','data:create','data:write'],
+        public:['viewables:read']
+    }
+}
